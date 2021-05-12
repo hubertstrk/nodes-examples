201158fe0f6c6cac28b875eae76d0a085b1034c0
Hubert <hubert.staerk@gmail.com>
2021-05-12 12:23:51 +0200

    add lodash grouping example

--- grouping/index.js
+++ grouping/index.js
@@ -1,36 +1,31 @@
-const {groupBy, minBy, keyBy} = require('lodash')
-
-const data = [
-  { user: 'a', value: 10 },
-  { user: 'a', value: 12 },
-  { user: 'b', value: 20 },
-  { user: 'c', value: 30 },
-  { user: 'c', value: 32 }
-]
+const {groupBy, inRange} = require('lodash')
 
 const data1 = [
-  { id: 'e65b1798-b412-4517-8bb7-bcfb947cddc0', value: 10 },
-  { id: 'ecf9f671-ff3d-403d-b849-1f4832af9e47', value: 12 },
-  { id: 'd87e6300-7bdb-4749-9831-5b1a8df1e2b5', value: 20 },
-  { id: 'd3a2dac9-ac5a-4341-bd16-eb49b9571dc4', value: 30 },
-  { id: 'edcac6a9-93bc-494f-b219-91648a194c8b', value: 32 }
+  { id: '1', value: 10, area: 3 },
+  { id: '2', value: 12, area: 4 },
+  { id: '3', value: 20, area: 3 },
+  { id: '4', value: 30, area: 7 },
+  { id: '5', value: 32, area: 1 },
+  { id: '6', value: 27, area: 3 },
+  { id: '7', value: 20, area: 3 },
+  { id: '8', value: 4, area: 4 },
+  { id: '9', value: 32, area: 5 },
+  { id: '10', value: 6, area: 3 },
+  { id: '11', value: 8, area: 3 },
+  { id: '12', value: 1, area: 6 },
+  { id: '13', value: 1, area: 3 },
+  { id: '14', value: 2, area: 2 }
 ]
 
+// {"D":[{"id":"1","value":10},{"id":"2","value":12},{"id":"3","value":20},{"id":"4","value":30},{"id":"5","value":32},{"id":"6","value":27},{"id":"7","value":20},{"id":"8","value":4},{"id":"9","value":32},{"id":"10","value":6},{"id":"11","value":8},{"id":"12","value":1},{"id":"13","value":1},{"id":"14","value":2}]}
 
-// const grouped = groupBy(data1, 'id')
-// console.info(`grouped: ${JSON.stringify(grouped)}`)
+const grouped = groupBy(data1, (x) => {
+  if (inRange(x.value, 8)) return 'A'
+  if (inRange(x.value, 8, 16)) return 'B'
+  if (inRange(x.value, 16, 30)) return 'C'
+  return 'D'
+})
 
+console.info(Object.keys(grouped))
 
-const keyed = keyBy(data1, x => x.id);
-console.info(JSON.stringify(keyed))
-
-
-// console.info(`grouped length: ${Object.keys(grouped).length}`)
-
-// const reducer = (acc, curr) => acc + curr.value;
-// const sum_result = Object.keys(grouped).map(x => grouped[x].reduce(reducer, 0))
-// console.info(sum_result)
-
-// const min_result = Object.keys(grouped).map(x => minBy(grouped[x], 'value')).map(x => x.value)
-// console.info(min_result)
-
+grouped.map(g =>  g.values)
